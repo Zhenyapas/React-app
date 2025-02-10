0cf63a32c29a05f2c4be461393132eda5bf8ac92
Zhenyapas <zhekazhekapas@gmail.com>
2025-02-10 12:02:07 +0200

    Updates

---
+++ precache-manifest.a3257c42c7ba6c2deff999a70b92e1dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecd485453ee7f8cf9e7f0fe011c0c71",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "0e24171b8f4cbb8b2ae2",
+    "url": "/React-app/static/css/main.ae9de6f1.chunk.css"
+  },
+  {
+    "revision": "0abcdb6a9c7d25f7d1c2",
+    "url": "/React-app/static/js/2.37317638.chunk.js"
+  },
+  {
+    "revision": "e29d32a2b0fad4af6122b4267d067932",
+    "url": "/React-app/static/js/2.37317638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af96dd43b94f5a241bc",
+    "url": "/React-app/static/js/3.6a4b1f65.chunk.js"
+  },
+  {
+    "revision": "0e24171b8f4cbb8b2ae2",
+    "url": "/React-app/static/js/main.e72cb7d3.chunk.js"
+  },
+  {
+    "revision": "499dac01ff42a179b736",
+    "url": "/React-app/static/js/runtime-main.8bd1a4b6.js"
+  },
+  {
+    "revision": "cdf0589adbbda9ef0d07da4bfa129d7a",
+    "url": "/React-app/static/media/loader.cdf0589a.svg"
+  },
+  {
+    "revision": "1eb4519054004be36177179c88c1f320",
+    "url": "/React-app/static/media/userFace.1eb45190.gif"
+  }
+]);
